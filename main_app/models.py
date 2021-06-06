c8934d727634aa0da5d3079163ca6d99c1687454
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-06-06 08:08:22 -0400

    [models] jsonfield

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models, migrations
+import jsonfield
 
 class Bubble(models.Model):
-    longitude = models.FloatField
-    latitude = models.FloatField
-    boardings = models.FloatField
-    alightings = models.FloatField
     stop_id = models.IntegerField
+    boardings = models.DecimalField
+    alightings = models.DecimalField
+    location = jsonfield.JSONField()
 
     def __str__(self):
         return self.stop_id
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 import csv, io
-from django.shortcuts import render
+import pandas as pd
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import permission_required
 from .models import Bubble
@@ -30,15 +31,26 @@ def data_upload(request):
     if not csv_file.name.endswith('.csv'):
         messages.error(request, 'File must be in .csv format.')
 
-    data_set = csv_file.read().decode('utf-8')
-    io_string = io.StringIO(data_set)
-    next(io_string)
-    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+    # data_set = csv_file.read().decode('UTF-8')
+    # io_string = io.StringIO(data_set)
+    # next(io_string)
+    data_set = pd.read_csv(csv_file)
+    # location_set = data_set['location']
+    # print(location_set)
+
+    for row in data_set:
         _, created = Bubble.objects.update_or_create(
-            stop_id=column[0],
-            boardings=column[1],
-            alightings=column[2],
-            location=column[3],
-            )
+            stop_id=data_set['stop_id'],
+            boardings=data_set['boardings'],
+            alightings=data_set['alightings'],
+            location=data_set['location']
+        )
+    # for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+    #     _, created = Bubble.objects.update_or_create(
+    #         stop_id=column[0],
+    #         boardings=column[1],
+    #         alightings=column[2],
+    #         location=column[3],
+    #         )
     context = {}
     return render(request, template, context)
