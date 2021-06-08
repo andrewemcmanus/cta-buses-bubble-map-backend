210a15e6f6ef0880caccde7e9b2492730353df88
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-06-08 08:50:19 -0400

    postgres

--- main_app/views.py
+++ main_app/views.py
@@ -36,25 +36,43 @@ def data_upload(request):
     # io_string = io.StringIO(data_set)
     # next(io_string)
     data_set = pd.read_csv(csv_file)
-    stop_ids = data_set['stop_id'].tolist()
+    # stop_ids = data_set['stop_id'].tolist()
     # can these simply be separated from panda series into lists, and then written to the database below?
     # rather than iterating through the CSV as is?
     # location_set = data_set['location']
     # print(location_set)
+    # stopvals = stops.astype(int)
+    # boardvals = boards.apply(lambda x: float(x))
+    # alightvals = alights.apply(lambda x: float(x))
 
-    for row in data_set:
+    stops = data_set['stop_id'].astype(int)
+    stop_ids = stops.values
+    boards = data_set['boardings'].apply(lambda x: float(x))
+    boarding = boards.values
+    alights = data_set['alightings'].apply(lambda x: float(x))
+    alighting = alights.values
+    locations = data_set['location'].values
+
+    for i in range(len(stop_ids)):
         _, created = Bubble.objects.update_or_create(
-            stop_id = data_set['stop_id'].astype(int),
-            boardings = data_set['boardings'].apply(lambda x: float(x)),
-            alightings = data_set['alightings'].apply(lambda x: float(x)),
-            location = data_set['location']
+            stop_id = stop_ids[i],
+            boardings = boarding[i],
+            alightings = alighting[i],
+            location = locations[i],
         )
-    # for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+    # for rows in data_set:
     #     _, created = Bubble.objects.update_or_create(
-    #         stop_id=column[0],
-    #         boardings=column[1],
-    #         alightings=column[2],
-    #         location=column[3],
+    #         stop_id = data_set['stop_id'].astype(int),
+    #         boardings = data_set['boardings'].apply(lambda x: float(x)),
+    #         alightings = data_set['alightings'].apply(lambda x: float(x)),
+    #         location = data_set['location']
+    #     )
+    # for rows in csv.reader(data_set):
+    #     _, created = Bubble.objects.update_or_create(
+    #         stop_id = data_set['stop_id'].astype(int),
+    #         boardings = data_set['boardings'].apply(lambda x: float(x)),
+    #         alightings = data_set['alightings'].apply(lambda x: float(x)),
+    #         location = data_set['location']
     #         )
     context = {}
     return render(request, template, context)
