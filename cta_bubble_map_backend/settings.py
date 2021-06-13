795a9dc203d5c4bea6cdbe3e99cd057f92908acd
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-06-13 15:10:30 -0400

    postgres migrations

--- cta_bubble_map_backend/settings.py
+++ cta_bubble_map_backend/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'cta_bubble_map_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cta_bubble_map',
     }
 }
 
